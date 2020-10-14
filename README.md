b9e697734ebce4992d6564379582726c362c38fe
Tobias Felber <tobias.felber97@gmail.com>
2020-10-14 20:31:47 +0200

    added WebConfig added app-routing fixed jar

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MyBudget
 
+mvn clean install
+-DskipTests
+
+java -jar backend/target/backend-[VERSION].jar
+
--- backend/src/main/java/ch/toubidev/backend/config/WebConfig.java
+++ backend/src/main/java/ch/toubidev/backend/config/WebConfig.java
@@ -1,18 +1,48 @@
 package ch.toubidev.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 
-public class WebConfig implements WebMvcConfigurer {
+@Configuration
+@EnableSwagger2
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("MyBudget REST API")
+                .description("\"Spring Boot REST API for MyBudget\"")
+                .version("${project.version}")
+                .license("GPL-3.0 License")
+                .contact(new Contact("Tobias Felber", null, "tobias.felber97@gmail.com"))
+                .build();
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
@@ -24,6 +54,11 @@ public class WebConfig implements WebMvcConfigurer {
                                 : new ClassPathResource("/static/index.html");
                     }
                 });
-    }
 
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
